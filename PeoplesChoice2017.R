5d69a9932a964d692080824d8a1b7d8c968c5e5c
suzan baert <suzan.baert@outlook.com>
2018-01-01 23:55:36 +0100

    added joined tables

--- PeoplesChoice2017.R
+++ PeoplesChoice2017.R
@@ -41,11 +41,13 @@ stubru_title <- stubru_html %>%
 
 stubru_ranking <- stubru_html %>%
   html_nodes(css =".song-position") %>%
-  html_text() 
+  html_text() %>%
+  as.numeric()
 
 
 #making a dataframe for studio brussels
-stubru <- data.frame(stubru_ranking, stubru_artist, stubru_title)
+stubru <- data.frame(stubru_ranking, stubru_artist, stubru_title, stringsAsFactors = FALSE)
+colnames(stubru) <- c("stubru_ranking", "artist", "title")
 
 
 #######
@@ -75,9 +77,12 @@ mnm_artist <- mnm_html %>%
 
 mnm_ranking <- mnm_html %>%
   html_nodes(css =".song-position") %>%
-  html_text()
+  html_text() %>%
+  as.numeric()
 
-mnm <- data.frame(mnm_ranking, mnm_artist, mnm_title)
+
+mnm <- data.frame(mnm_ranking, mnm_artist, mnm_title, stringsAsFactors = FALSE)
+colnames(mnm) <- c("mnm_ranking", "artist", "title")
 
 
 
@@ -98,10 +103,76 @@ radio1_extract <- radio1_html %>%
   html_nodes(css =".track-row") %>%
   html_text() %>%
   tolower() %>%
-  as.data.frame() 
+  as.data.frame()
 
-#cleaning up the dataframe
-radio1 <- tidyr::separate(radio1_extract, 1, into= c("ranking","title", "artist"), sep="\n")
-radio1$title <- str_trim(radio1$artist, side="both")
-radio1$artist <- str_trim(radio1$title, side="both")
-radio1$artist <- str_replace(radio1$title, "[0-9]+:[0-9]{2}", "")
+#cleaning up the dataframe-
+radio1 <- tidyr::separate(radio1_extract, 1, into= c("radio1_ranking","title", "artist"), sep="\n")
+radio1$title <- str_trim(radio1$title, side="both")
+radio1$artist <- str_trim(radio1$artist, side="both")
+radio1$artist <- str_replace(radio1$artist, "[0-9]+:[0-9]{2}", "")
+radio1$radio1_ranking <- as.numeric(radio1$radio1_ranking)
+
+
+
+
+
+#commonalities
+stubru %>%
+  semi_join(radio1, by="title") %>%
+  semi_join(mnm, by="title")
+
+
+stubru %>%
+  semi_join(radio1, by="title")
+
+stubru %>%
+  semi_join(mnm, by="title")
+
+mnm %>%
+  semi_join(radio1, by="title")
+
+
+#joining them all.
+#can't join artist as well due to inconsistencies in naming artists, see chainsmokers and coldplay as example
+all_stations_messy <- stubru %>%
+  full_join(mnm, by="title") %>%
+  full_join(radio1, by="title")
+
+#if empty take the artist name from the other list
+all_stations_messy$artist.x <- ifelse(!is.na(all_stations_messy$artist.x), all_stations_messy$artist.x, all_stations_messy$artist.y)
+all_stations_messy$artist.x <- ifelse(!is.na(all_stations_messy$artist.x), all_stations_messy$artist.x, all_stations_messy$artist)
+all_stations_messy$artist <- all_stations_messy$artist.x
+
+all_stations <- all_stations_messy %>%
+  select(artist, title, stubru_ranking, mnm_ranking, radio1_ranking)
+
+
+all_stations_messy$artistjoin <- ifelse(is.na(all_stations_messy$artist.x), 
+                                        ifelse(is.na(all_stations_messy$artist.y), all_stations_messy$artist, 
+                                        all_stations_messy$artist.y),
+                                        all_stations_messy$artist.x)
+
+str(all_stations)
+
+
+#quick plot
+library(ggplot2)
+ggplot(all_stations) +
+  geom_point(aes(x=stubru_ranking, y=radio1_ranking)) +
+  scale_x_reverse(name = "Studio Brussels ranking") +
+  scale_y_reverse(name = "Radio 1 ranking")
+
+
+stubru %>%
+  inner_join(radio1, by="title") %>%
+  ggplot()+
+    geom_point(aes(x=stubru_ranking, y=radio1_ranking)) +
+    scale_x_reverse(name = "Studio Brussels ranking") +
+    scale_y_reverse(name = "Radio 1 ranking") +
+    geom_text(aes(x=stubru_ranking, y=radio1_ranking, label = title))
+
+
+all_stations %>%
+  filter(title == "humble.")
+
+  
